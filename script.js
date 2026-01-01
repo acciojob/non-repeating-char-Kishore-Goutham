8f924e1767bfa07934c8cd742e47197641d19cb1
Kishore-Goutham <kishoregoutham123@gmail.com>
2026-01-02 00:14:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
+	
  // Write your code here
+	let m = new Map();
+	for(let i=0;i<str.length;i++){
+
+		if(m.has(str[i])){
+			m.set(str[i], m.get(str[i])+1)
+		}else{
+			m.set(str[i],1)
+		}
+	}
+
+	for(let key of m.keys()){
+		if(m.get(key)==1){
+			return key;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
